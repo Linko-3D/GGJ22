1d62cc273d12ab76709a7ca01745c66e66335b67
Linko-3D <dbekhouc@gmail.com>
2022-01-23 17:34:11 +0100

    tweak

--- singletons/Network.gd
+++ singletons/Network.gd
@@ -3,9 +3,14 @@ extends Node
 func create_server():
 	var peer = ENetMultiplayerPeer.new()
 	print(peer)
+	load_game()
 
 func join_server():
 	print("b")
+	load_game()
+
+func load_game():
+	get_tree().change_scene("res://scenes/map.tscn")
 
 func get_IP():
 	print(IP.get_local_addresses())
