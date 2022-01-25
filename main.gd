148635030c0f15d7aaa3c9f91af30ee858057f6d
Linko-3D <dbekhouc@gmail.com>
2022-01-26 00:17:08 +0100

    tweak

--- main.gd
+++ main.gd
@@ -2,21 +2,19 @@ extends Control
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	$Credit.modulate = Color(1, 1, 1, 0)
-	$Menu.modulate =  Color(1, 1, 1, 0)
-
-	var tween = create_tween()
-	tween.tween_property($Menu, "modulate", Color(1, 1, 1, 1), 0.5)
-	tween.tween_property($Credit, "modulate", Color(1, 1, 1, 1), 0.5).set_delay(0.5)
-	tween.set_parallel().tween_property($Credit, "rect_position:x", 20.0, 0.5).set_delay(0.5)
-
-	$Menu/LocalIP.text = "My local IP: " + Network.get_IP()
-
-func _on_quit_pressed():
-	get_tree().quit()
 
 func _on_host_pressed():
 	Network.create_server()
 
 func _on_join_pressed():
-	Network.join_server()
+	if $Menu/ToLocalIP.text == "":
+		Network.join_server($Menu/ToLocalIP.placeholder_text)
+	else:
+		Network.join_server($Menu/ToLocalIP.text)
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_to_local_ip_text_submitted(new_text):
+	_on_join_pressed()
--- singletons/Network.gd
+++ singletons/Network.gd
@@ -1,19 +1,58 @@
 extends Node
 
-func create_server():
-	var peer = ENetMultiplayerPeer.new()
-	print(peer)
-	load_game()
+var map = "res://scenes/map.tscn"
+var player = preload("res://player/player.tscn")
 
-func join_server():
-	print("b")
-	load_game()
+var peer = ENetMultiplayerPeer.new()
 
-func load_game():
-	get_tree().change_scene("res://scenes/map.tscn")
+func _ready():	
+	peer.peer_connected.connect(_peer_connected)
+	peer.peer_disconnected.connect(_peer_disconnected)
+	peer.connection_succeeded.connect(_connection_succeeded)
 
 func get_IP():
-	print(IP.get_local_addresses())
 	for ip in IP.get_local_addresses():
 		if ip.begins_with("192.168."):
 			return ip
+
+func create_server():
+	peer.create_server(4242)
+	multiplayer.set_multiplayer_peer(peer)
+
+	load_game()
+
+func join_server(ip):
+	peer.create_client(ip, 4242)
+	multiplayer.set_multiplayer_peer(peer)
+
+func _connection_succeeded():
+	load_game()
+
+func load_game():
+	get_tree().change_scene(map)
+	
+	await get_tree().create_timer(0.01).timeout
+	
+	print( get_tree().get_root().find_node("Spawn", true, false) )
+	
+	if not multiplayer.is_server():
+		spawn_player(multiplayer.get_unique_id())
+	else:
+		var message = Label.new()
+		message.text = "    Server local IP:    " + get_IP()
+		add_child(message)
+	
+func spawn_player(id):
+	var player_instance = player.instantiate()
+	player_instance.name = str(id)
+	add_child(player_instance)
+	
+	player_instance.set_multiplayer_authority(id)
+
+func _peer_connected(id):
+	if id != 1:
+		spawn_player(id)
+
+func _peer_disconnected(id):
+	if id != 1:
+		get_tree().get_root().find_node(str(id), true, false).queue_free()
