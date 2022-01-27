d1dd12f7c0a3b7032d64b6a0423c5aa0ae13e5ef
Linko-3D <dbekhouc@gmail.com>
2022-01-27 03:06:27 +0100

    tweak

--- main.gd
+++ main.gd
@@ -5,13 +5,15 @@ func _ready():
 	$Hide.show()
 	$Credit.modulate = Color(1, 1, 1, 0)
 	$Menu.modulate = Color(1, 1, 1, 0)
+	$Credit.percent_visible = 0
 
 	var tween = create_tween()
 	tween.tween_property($Hide, "color", Color(0, 0, 0, 0), 0.2)
 	tween.tween_property($Menu, "modulate", Color(1, 1, 1, 1), 0.2)
 	tween.tween_callback($Hide.hide)
 	tween.tween_property($Credit, "offset_left", 20.0, 1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	tween.set_parallel().tween_property($Credit, "modulate", Color(1, 1, 1, 1), 0.5)
+	tween.set_parallel().tween_property($Credit, "modulate", Color(1, 1, 1, 1), 1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.set_parallel().tween_property($Credit, "percent_visible", 1.0, 1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 
 func _on_host_pressed():
 	Network.create_server()
