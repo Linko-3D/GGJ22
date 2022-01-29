fa480d619d0d56c3bc74d2eebbb2f7f5ab3d23de
Linko-3D <dbekhouc@gmail.com>
2022-01-29 01:40:45 +0100

    tweak

---
+++ scenes/splash_screen/splash_screen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _ready():
+	$Hide.show()
+
+	var tween = create_tween()
+	tween.tween_property($Hide, "color", Color(0, 0, 0, 0), 0.2)
+	tween.tween_property($Hide, "color", Color(0, 0, 0, 1), 0.2).set_delay(1)
+	tween.tween_callback(load_scene)
+
+func load_scene():
+	get_tree().change_scene("res://main.tscn")
